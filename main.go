c9a34882617bc191601c94f502eebec70a243b56
Gustav Behm <me@rootmos.io>
2023-12-20 09:14:11 +0100

    Create empty tarball

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"bufio"
+	"io"
+	"archive/tar"
 
 	"rootmos.io/sitepkg/internal/common"
 )
@@ -31,9 +33,19 @@ func Load(path string) (m *Manifest, err error) {
 	return m, nil
 }
 
+func (m *Manifest) CreateTarball(w io.Writer) (err error) {
+	t := tar.NewWriter(w)
+	defer func() {
+		err = t.Close()
+	}()
+
+	return
+}
+
 func main() {
 	chrootFlag := flag.String("chroot", common.Getenv("CHROOT"), "act relative directory")
 	manifestFlag := flag.String("manifest", common.Getenv("MANIFEST"), "manifest path")
+	outputFlag := flag.String("output", common.Getenv("OUTPUT"), "write tarball to path")
 	flag.Parse()
 
 	root := *chrootFlag
@@ -59,4 +71,19 @@ func main() {
 	}
 
 	log.Print(m)
+
+	if *outputFlag == "" {
+		log.Fatal("output not specified")
+	}
+
+	f, err := os.Create(*outputFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	err = m.CreateTarball(f)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
