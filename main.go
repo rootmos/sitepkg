d055f40053229d22664f480a439c0e4695ce5ccc
Gustav Behm <me@rootmos.io>
2023-12-20 08:57:58 +0100

    Load a manifest

--- main.go
+++ main.go
@@ -5,12 +5,35 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+	"bufio"
 
 	"rootmos.io/sitepkg/internal/common"
 )
 
+type Manifest struct {
+	Paths []string
+}
+
+func Load(path string) (m *Manifest, err error) {
+	f, err := os.Open(path)
+	defer f.Close()
+
+	m = &Manifest{}
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		m.Paths = append(m.Paths, s.Text())
+	}
+	if err = s.Err(); err != nil {
+		return
+	}
+
+	return m, nil
+}
+
 func main() {
 	chrootFlag := flag.String("chroot", common.Getenv("CHROOT"), "act relative directory")
+	manifestFlag := flag.String("manifest", common.Getenv("MANIFEST"), "manifest path")
 	flag.Parse()
 
 	root := *chrootFlag
@@ -25,4 +48,15 @@ func main() {
 	}
 
 	log.Printf("root: %v", root)
+
+	if *manifestFlag == "" {
+		log.Fatal("manifest not specified")
+	}
+
+	m, err := Load(*manifestFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(m)
 }
